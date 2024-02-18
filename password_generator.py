7095bf5db779b430a94d816697136c285d37281c
Dsong805 <schischov803@mail.ru>
2024-02-18 20:35:19 +0300

    Expanded character set for password generation

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,10 @@
- 
+import random
+import string
+
+def generate_password(length=11):
+    characters = string.ascii_letters
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+if __name__ == __main__:
+    print(Generated password:, generate_password()) 
