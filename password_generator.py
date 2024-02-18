b3267eee3f6c0376572aa5061abe0d0ac5b18f16
Dsong805 <schischov803@mail.ru>
2024-02-18 20:37:06 +0300

    Expanded character set for password generation to include uppercase letters

--- password_generator.py
+++ password_generator.py
@@ -2,9 +2,10 @@ import random
 import string
 
 def generate_password(length=11):
-    characters = string.ascii_letters
+    characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
     return password
 
 if __name__ == __main__:
-    print(Generated password:, generate_password()) 
+    print(Generated password:, generate_password())
+ 
